55a64b1ca29006d8d34d71f3a05c93c73fd4dbba
Yuki Kishimoto <yukikishimoto@proton.me>
2022-09-12 21:58:30 +0200

    Update example

--- examples/hello_world_alert.rs
+++ examples/hello_world_alert.rs
@@ -6,12 +6,18 @@ use ntfy::{Dispatcher, Payload, Priority};
 fn main() {
     let dispatcher = Dispatcher::new("https://ntfy.sh", Some("socks5h://127.0.0.1:9050")).unwrap();
 
-    let payload = Payload {
+    /* let payload = Payload {
         topic: String::from("mytopic"),
         message: String::from("Hello, World!"),
         priority: Priority::Default,
         title: Some(String::from("Alert")),
-    };
+    }; */
+
+    // or
+
+    let payload = Payload::new("mytopic", "Hello, World!")
+        .title("Alert") // Add optional title
+        .priority(Priority::High); // Edit priority
 
     dispatcher.send(&payload).unwrap();
 }
