a5aaa81b9af8d9bfc381b907dcb9d9dfd84768e8
Yuki Kishimoto <yukikishimoto@proton.me>
2022-11-10 10:19:48 +0100

    Add thiserror

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ use reqwest::blocking::Client as ReqwestClient;
 #[cfg(feature = "blocking")]
 use reqwest::blocking::RequestBuilder;
 use reqwest::{Proxy, StatusCode};
+use thiserror::Error;
 
 mod priority;
 
@@ -59,30 +60,47 @@ impl Payload {
     }
 }
 
-#[derive(Debug)]
-pub enum Error {
+#[derive(Debug, Error)]
+pub enum NtfyError {
+    #[error("Failed to deserialize: {0}")]
     FailedToDeserialize(String),
+    #[error("Reqwest error: {0}")]
     ReqwestError(reqwest::Error),
+    #[error("Empty Response")]
     EmptyResponse,
+    #[error("Bad Result")]
     BadResult,
+    #[error("Unauthorized")]
     Unauthorized,
+    #[error("Bad Request")]
     BadRequest,
+    #[error("Forbidden")]
     Forbidden,
+    #[error("Not Found")]
     NotFound,
+    #[error("Method Not Allowed")]
     MethodNotAllowed,
+    #[error("Too Many Requests")]
     TooManyRequests,
+    #[error("Unhandled Client Error")]
     UnhandledClientError,
+    #[error("Internal Server Error")]
     InternalServerError,
+    #[error("Internal Server Error")]
     NotImplemented,
+    #[error("Bad Gateway")]
     BadGateway,
+    #[error("Service Unavailable")]
     ServiceUnavailable,
+    #[error("Gateway Timeout")]
     GatewayTimeout,
+    #[error("Unhandled Server Error")]
     UnhandledServerError,
 }
 
 impl Dispatcher {
     /// Create new dispatcher
-    pub fn new(url: &str, proxy: Option<&str>) -> Result<Self, Error> {
+    pub fn new(url: &str, proxy: Option<&str>) -> Result<Self, NtfyError> {
         let mut client = ReqwestClient::builder();
 
         if let Some(proxy) = proxy {
@@ -97,21 +115,21 @@ impl Dispatcher {
 
     /// Send payload to ntfy server
     #[cfg(not(feature = "blocking"))]
-    pub async fn send(&self, payload: &Payload) -> Result<(), Error> {
+    pub async fn send(&self, payload: &Payload) -> Result<(), NtfyError> {
         log::debug!("{:?}", payload);
         request(self.client.post(&self.url).json(payload)).await
     }
 
     /// Send payload to ntfy server
     #[cfg(feature = "blocking")]
-    pub fn send(&self, payload: &Payload) -> Result<(), Error> {
+    pub fn send(&self, payload: &Payload) -> Result<(), NtfyError> {
         log::debug!("{:?}", payload);
         request(self.client.post(&self.url).json(payload))
     }
 }
 
 #[cfg(not(feature = "blocking"))]
-async fn request(req: RequestBuilder) -> Result<(), Error> {
+async fn request(req: RequestBuilder) -> Result<(), NtfyError> {
     let res = req.send().await?;
 
     match StatusCode::as_u16(&res.status()) {
@@ -119,26 +137,26 @@ async fn request(req: RequestBuilder) -> Result<(), Error> {
             let res = res.text().await?;
 
             if res.is_empty() {
-                return Err(Error::EmptyResponse);
+                return Err(NtfyError::EmptyResponse);
             }
 
             Ok(())
         }
-        400 => Err(Error::BadRequest),
-        401 => Err(Error::Unauthorized),
-        402 => Err(Error::UnhandledClientError),
-        403 => Err(Error::Forbidden),
-        404 => Err(Error::NotFound),
-        405 => Err(Error::MethodNotAllowed),
-        406_u16..=428_u16 => Err(Error::UnhandledClientError),
-        429 => Err(Error::TooManyRequests),
-        430_u16..=499_u16 => Err(Error::UnhandledClientError),
-        500 => Err(Error::InternalServerError),
-        501 => Err(Error::NotImplemented),
-        502 => Err(Error::BadGateway),
-        503 => Err(Error::ServiceUnavailable),
-        504 => Err(Error::GatewayTimeout),
-        _ => Err(Error::UnhandledServerError),
+        400 => Err(NtfyError::BadRequest),
+        401 => Err(NtfyError::Unauthorized),
+        402 => Err(NtfyError::UnhandledClientError),
+        403 => Err(NtfyError::Forbidden),
+        404 => Err(NtfyError::NotFound),
+        405 => Err(NtfyError::MethodNotAllowed),
+        406_u16..=428_u16 => Err(NtfyError::UnhandledClientError),
+        429 => Err(NtfyError::TooManyRequests),
+        430_u16..=499_u16 => Err(NtfyError::UnhandledClientError),
+        500 => Err(NtfyError::InternalServerError),
+        501 => Err(NtfyError::NotImplemented),
+        502 => Err(NtfyError::BadGateway),
+        503 => Err(NtfyError::ServiceUnavailable),
+        504 => Err(NtfyError::GatewayTimeout),
+        _ => Err(NtfyError::UnhandledServerError),
     }
 }
 
@@ -151,31 +169,31 @@ fn request(req: RequestBuilder) -> Result<(), Error> {
             let res = res.text()?;
 
             if res.is_empty() {
-                return Err(Error::EmptyResponse);
+                return Err(NtfyError::EmptyResponse);
             }
 
             Ok(())
         }
-        400 => Err(Error::BadRequest),
-        401 => Err(Error::Unauthorized),
-        402 => Err(Error::UnhandledClientError),
-        403 => Err(Error::Forbidden),
-        404 => Err(Error::NotFound),
-        405 => Err(Error::MethodNotAllowed),
-        406_u16..=428_u16 => Err(Error::UnhandledClientError),
-        429 => Err(Error::TooManyRequests),
-        430_u16..=499_u16 => Err(Error::UnhandledClientError),
-        500 => Err(Error::InternalServerError),
-        501 => Err(Error::NotImplemented),
-        502 => Err(Error::BadGateway),
-        503 => Err(Error::ServiceUnavailable),
-        504 => Err(Error::GatewayTimeout),
-        _ => Err(Error::UnhandledServerError),
+        400 => Err(NtfyError::BadRequest),
+        401 => Err(NtfyError::Unauthorized),
+        402 => Err(NtfyError::UnhandledClientError),
+        403 => Err(NtfyError::Forbidden),
+        404 => Err(NtfyError::NotFound),
+        405 => Err(NtfyError::MethodNotAllowed),
+        406_u16..=428_u16 => Err(NtfyError::UnhandledClientError),
+        429 => Err(NtfyError::TooManyRequests),
+        430_u16..=499_u16 => Err(NtfyError::UnhandledClientError),
+        500 => Err(NtfyError::InternalServerError),
+        501 => Err(NtfyError::NotImplemented),
+        502 => Err(NtfyError::BadGateway),
+        503 => Err(NtfyError::ServiceUnavailable),
+        504 => Err(NtfyError::GatewayTimeout),
+        _ => Err(NtfyError::UnhandledServerError),
     }
 }
 
-impl From<reqwest::Error> for Error {
+impl From<reqwest::Error> for NtfyError {
     fn from(err: reqwest::Error) -> Self {
-        Error::ReqwestError(err)
+        NtfyError::ReqwestError(err)
     }
 }
