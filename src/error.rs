0f5051e3952de626eb41ee377d2f8794a5dff54e
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-01-02 10:36:24 +0100

    Remove `thiserror` dep

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,45 @@
 // Copyright (c) 2022 Yuki Kishimoto
 // Distributed under the MIT software license
 
-use reqwest::header::InvalidHeaderValue;
-use thiserror::Error;
+use std::fmt;
 
-#[derive(Debug, Error)]
+use reqwest::header::InvalidHeaderValue;
+
+#[derive(Debug)]
 pub enum NtfyError {
-    #[error(transparent)]
-    ReqwestError(#[from] reqwest::Error),
-    #[error(transparent)]
-    Url(#[from] url::ParseError),
-    #[error(transparent)]
-    InvalidHeaderValue(#[from] InvalidHeaderValue),
-    #[error("Empty Response")]
+    ReqwestError(reqwest::Error),
+    Url(url::ParseError),
+    InvalidHeaderValue(InvalidHeaderValue),
     EmptyResponse,
+}
+
+impl std::error::Error for NtfyError {}
+
+impl fmt::Display for NtfyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::ReqwestError(e) => write!(f, "{}", e),
+            Self::Url(e) => write!(f, "{}", e),
+            Self::InvalidHeaderValue(e) => write!(f, "{}", e),
+            Self::EmptyResponse => write!(f, "Empty Response"),
+        }
+    }
+}
+
+impl From<reqwest::Error> for NtfyError {
+    fn from(e: reqwest::Error) -> Self {
+        Self::ReqwestError(e)
+    }
+}
+
+impl From<url::ParseError> for NtfyError {
+    fn from(e: url::ParseError) -> Self {
+        Self::Url(e)
+    }
+}
+
+impl From<InvalidHeaderValue> for NtfyError {
+    fn from(e: InvalidHeaderValue) -> Self {
+        Self::InvalidHeaderValue(e)
+    }
 }
